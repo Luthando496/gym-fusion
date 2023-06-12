beba813011583c241782237c9bc095d2a1d6e4bc
unknown <luthandodidiza197@gmail.com>
2023-06-12 10:16:15 +0200

    added pricing chart

--- src/components/PricingChart.jsx
+++ src/components/PricingChart.jsx
@@ -9,7 +9,99 @@ const PricingChart = () => {
     </h2>
     </div>
     <h3 className="text-center text-2xl capitalize mt-12 font-semibold">Exclusive Pricing Plan</h3>
-    <p className="text-center text-md tracking-[1.8] leading-[1.8] text-gray-500 my-8">Gymat an unknown printer took a galley of type and scrambled <br className='hidden md:block'/> make a type specimen book.</p>
+    <p className="text-center text-md tracking-[1.8] leading-[1.8] text-gray-500 my-8">Gym an unknown printer took a galley of type and scrambled <br className='hidden md:block'/> make a type specimen book.</p>
+
+    <div className="my-14 grid grid-cols-1 lg:grid-cols-3 gap-5 px-16">
+
+    {/* card */}
+    <div className="card group w-full shadow-2xl ">
+        <div className="img w-full h-[380px] relative">
+            <img src="https://images.pexels.com/photos/2827400/pexels-photo-2827400.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" className="h-full w-full object-cover" />
+            <div className='absolute bg-white text-[20px] font-bold w-[18rem] text-center py-6 text-[#ff0336] -bottom-[18px] left-0 right-0 mx-auto'>Beginner</div>
+        </div>
+        
+        <div className="flex flex-col items-center pt-[20px] pb-[50px]">
+        <p className="text-center text-[55px] font-bold relative py-[10px] ">
+        <span className="text-[30px] text-[#6d6d6d] absolute font-normal top-4 -left-[1rem]">$</span>19
+        <span className="text-[30px] text-[#6d6d6d] absolute font-normal bottom-4 -right-[4rem]">p/m</span>
+        </p>
+        <div className="flex flex-col gap-4">
+        <p className="text-center font-light text-xl text-gray-400">Free hand</p>
+        <p className="text-center font-light text-xl text-gray-400">Gym Fitness</p>
+        <p className="text-center font-light text-xl text-gray-400">Weight Loss</p>
+        <p className="text-center font-light text-xl text-gray-400">Personal Trainer</p>
+        <p className="text-center font-light text-xl text-gray-400">Cycling</p>
+        </div>
+
+        <div className="mt-8 flex justify-center">
+
+        <button className="px-6 uppercase tracking-[2px] text-xl font-bold py-4 text-white bg-yellow-400 btn-card hover:bg-pink-400 duration-700">Purchase Now</button>
+
+        </div>
+
+        </div>
+    </div>
+    {/* card end */}
+     {/* card */}
+     <div className="card group w-full shadow-2xl ">
+        <div className="img w-full h-[380px] relative">
+            <img src="https://images.pexels.com/photos/13018347/pexels-photo-13018347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" className="h-full w-full object-cover" />
+            <div className='absolute bg-white text-[20px] font-bold w-[18rem] text-center py-6 text-[#ff0336] -bottom-[18px] left-0 right-0 mx-auto'>Beginner</div>
+        </div>
+        
+        <div className="flex flex-col items-center pt-[20px] pb-[50px]">
+        <p className="text-center text-[55px] font-bold relative py-[10px] ">
+        <span className="text-[30px] text-[#6d6d6d] absolute font-normal top-4 -left-[1rem]">$</span>79
+        <span className="text-[30px] text-[#6d6d6d] absolute font-normal bottom-4 -right-[4rem]">p/m</span>
+        </p>
+        <div className="flex flex-col gap-4">
+        <p className="text-center font-light text-xl text-gray-400">Free hand</p>
+        <p className="text-center font-light text-xl text-gray-400">Gym Fitness</p>
+        <p className="text-center font-light text-xl text-gray-400">Weight Loss</p>
+        <p className="text-center font-light text-xl text-gray-400">Personal Trainer</p>
+        <p className="text-center font-light text-xl text-gray-400">Cycling</p>
+        </div>
+
+        <div className="mt-8 flex justify-center">
+
+        <button className="px-6 uppercase tracking-[2px] text-xl font-bold py-4 text-white bg-yellow-400 btn-card hover:bg-pink-400 duration-700">Purchase Now</button>
+
+        </div>
+
+        </div>
+    </div>
+    {/* card end */}
+     {/* card */}
+     <div className="card group w-full shadow-2xl ">
+        <div className="img w-full h-[380px] relative">
+            <img src="https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" className="h-full w-full object-cover" />
+            <div className='absolute bg-white text-[20px] font-bold w-[18rem] text-center py-6 text-[#ff0336] -bottom-[18px] left-0 right-0 mx-auto'>Beginner</div>
+        </div>
+        
+        <div className="flex flex-col items-center pt-[20px] pb-[50px]">
+        <p className="text-center text-[55px] font-bold relative py-[10px] ">
+        <span className="text-[30px] text-[#6d6d6d] absolute font-normal top-4 -left-[1rem]">$</span>190
+        <span className="text-[30px] text-[#6d6d6d] absolute font-normal bottom-4 -right-[4rem]">p/m</span>
+        </p>
+        <div className="flex flex-col gap-4">
+        <p className="text-center font-light text-xl text-gray-400">Free hand</p>
+        <p className="text-center font-light text-xl text-gray-400">Gym Fitness</p>
+        <p className="text-center font-light text-xl text-gray-400">Weight Loss</p>
+        <p className="text-center font-light text-xl text-gray-400">Personal Trainer</p>
+        <p className="text-center font-light text-xl text-gray-400">Cycling</p>
+        </div>
+
+        <div className="mt-8 flex justify-center">
+
+        <button className="px-6 uppercase tracking-[2px] text-xl font-bold py-4 text-white bg-yellow-400 btn-card hover:bg-pink-400 duration-700">Purchase Now</button>
+
+        </div>
+
+        </div>
+    </div>
+    {/* card end */}
+
+    </div>
     </section>
   )
 }
